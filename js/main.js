6cc2edba71b3d5ef61658065ef17218856e71960
deanyosla <146840740+deanyosla@users.noreply.github.com>
2023-11-06 14:07:58 +0000

    adjustments

--- js/main.js
+++ js/main.js
@@ -6,6 +6,52 @@ hamburger.addEventListener('click', function () {
     menu_nav.classList.toggle("is-active");
 });
 
+// const headertxt = document.querySelector('.h1-text');
+// const characters = headertxt.innerHTML.split('');
+//       headertxt.innerHTML = '';
+//       characters.forEach((character, index) => {
+//         setTimeout(() => {
+//       headertxt.innerHTML += character;
+//   }, 100 * index);
+// });
+
+// const paragraph = document.querySelector('.p-text');
+// const chara = paragraph.innerHTML.split('');
+//         paragraph.innerHTML = '';
+//         chara.forEach((character, index) => {
+//             setTimeout(() => {
+//                 paragraph.innerHTML += character;
+//             }, 200 * index);
+//         });
+
+const headerTxt = "Hi, I'm Andrejs Volskis!";
+const paraTxt = 'I am an aspiring web developer.';
+let headerSpeed = 80;
+let paraSpeed = 60;
+let headerLength = 0;
+let paraLength = 0;
+
+function typeWriterHeader() {
+    if (headerLength < headerTxt.length) {
+        document.getElementById('main').innerHTML += headerTxt.charAt(headerLength);
+        headerLength++;
+        setTimeout(typeWriterHeader, headerSpeed);
+    }
+}
+function typeWriterPara() {
+    if (paraLength < paraTxt.length) {
+        document.getElementById('para-text').innerHTML += paraTxt.charAt(paraLength);
+        paraLength++;
+        setTimeout(typeWriterPara, paraSpeed);
+    }
+}
+
+function typeWriter() {
+    typeWriterHeader();
+    setTimeout(typeWriterPara, 3000);
+}
+
+typeWriter();
 
 // $('.project-card').slick({
 //     arrows: false,
