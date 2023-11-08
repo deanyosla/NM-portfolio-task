209c5a2bf72c4e7480c7e4119b23b51a9a1945e4
deanyosla <146840740+deanyosla@users.noreply.github.com>
2023-11-08 09:10:56 +0000

    Completed adding form validation

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ hamburger.addEventListener('click', function () {
 });
 
 
+
+
 const headerTxt = "Hi, I'm Andrejs Volskis!";
 const paraTxt = 'I am an aspiring Web Developer.';
 let headerSpeed = 80;
@@ -36,162 +38,85 @@ function typeWriter() {
 
 typeWriter();
 
- // Working submission form
+ // Working form validation
 
-function validateForm() {
-    const firstName = document.getElementById("f-name").value;
-    const lastName = document.getElementById("l-name").value;
-    const email = document.getElementById("email").value;
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const message = document.getElementById("msg-field").value;
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const firstName = document.getElementById("f-name");
+  const lastName = document.getElementById("l-name");
+  const email = document.getElementById("email");
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const message = document.getElementById("msg-field");
 
-    function displayErrorMessage(elementId, message) {
-        const errorElement = document.getElementById(elementId);
-        errorElement.textContent = message;
-        errorElement.style.display = "block";
-      
-        // Apply the error border class to the corresponding input field
-        const inputField = document.getElementById(elementId.replace("-error", ""));
-        inputField.classList.add("error-border");
+  form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevents the default form submission behavior
+
+      // Clears any previous error messages and removes error borders
+      clearErrorMessages();
+
+      let hasErrors = false;
+
+      if (firstName.value === "") {
+          displayErrorMessage("f-name-error", "*First Name is required.");
+          addErrorBorder("f-name");
+          hasErrors = true;
       }
-      
-      function clearErrorMessages() {
-        const errorElements = document.getElementsByClassName("error-message");
-        for (let i = 0; i < errorElements.length; i++) {
+
+      if (lastName.value === "") {
+          displayErrorMessage("l-name-error", "*Last Name is required.");
+          addErrorBorder("l-name");
+          hasErrors = true;
+      }
+
+      if (email.value === "") {
+          displayErrorMessage("email-error", "*Email Address is required.");
+          addErrorBorder("email");
+          hasErrors = true;
+      } else if (!email.value.match(emailPattern)) {
+          displayErrorMessage("email-error", "*Email Address is not valid.");
+          addErrorBorder("email");
+          hasErrors = true;
+      }
+
+      if (message.value.length < 40) {
+          displayErrorMessage("msg-field-error", "*Message should be at least 40 characters long.");
+          addErrorBorder("msg-field");
+          hasErrors = true;
+      }
+
+      if (hasErrors) {
+          return;
+      }
+
+      // Resets the form
+      form.reset();
+  });
+
+  function displayErrorMessage(elementId, message) {
+      const errorElement = document.getElementById(elementId);
+      errorElement.textContent = message;
+      errorElement.style.display = "block";
+  }
+
+  function addErrorBorder(inputId) {
+      const inputField = document.getElementById(inputId);
+      inputField.classList.add("error-border");
+  }
+
+  function clearErrorMessages() {
+      const errorElements = document.getElementsByClassName("error-message");
+      for (let i = 0; i < errorElements.length; i++) {
           errorElements[i].textContent = "";
           errorElements[i].style.display = "none";
-      
-          // Remove the error border class from all input fields
-          const inputField = document.getElementById(errorElements[i].id.replace("-error", ""));
-          inputField.classList.remove("error-border");
-        }
       }
-  
-    // Clear any previous error messages and remove error borders
-    clearErrorMessages();
-  
-    let hasErrors = false;
-  
-    if (firstName === "") {
-      displayErrorMessage("f-name-error", "*First Name is required.");
-      hasErrors = true;
-    }
-  
-    if (lastName === "") {
-      displayErrorMessage("l-name-error", "*Last Name is required.");
-      hasErrors = true;
-    }
-  
-    if (email === "") {
-      displayErrorMessage("email-error", "*Email Address is required.");
-      hasErrors = true;
-    } else if (!email.match(emailPattern)) {
-      displayErrorMessage("email-error", "*Email Address is not valid.");
-      hasErrors = true;
-    }
-  
-    if (message.length < 40 ) {
-      displayErrorMessage("msg-field-error", "*Message should be at least 40 characters long.");
-      hasErrors = true;
-    }
-  
-    if (hasErrors) {
-      return false;
-    }
-    form.reset();
-    return true;
+
+      const inputFields = document.querySelectorAll("input, #msg-field");
+      for (let i = 0; i < inputFields.length; i++) {
+          inputFields[i].classList.remove("error-border");
+      }
   }
-  
+});
 
-
-
-
-
-
-// Not working for now 
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const form = document.getElementById("contact-form");
-//     const firstName = document.getElementById("f-name");
-//     const lastName = document.getElementById("l-name");
-//     const email = document.getElementById("email");
-//     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//     const message = document.getElementById("msg-field");
-  
-//     form.addEventListener("submit", function (event) {
-//       event.preventDefault(); // Prevent the default form submission behavior
-  
-//       // Clear any previous error messages and remove error borders
-//       clearErrorMessages();
-  
-//       let hasErrors = false;
-  
-//       if (firstName.value === "") {
-//         displayErrorMessage("f-name-error", "*First Name is required.");
-//         addErrorBorder("f-name");
-//         hasErrors = true;
-//       }
-  
-//       if (lastName.value === "") {
-//         displayErrorMessage("l-name-error", "*Last Name is required.");
-//         addErrorBorder("l-name");
-//         hasErrors = true;
-//       }
-  
-//       if (email.value === "") {
-//         displayErrorMessage("email-error", "*Email Address is required.");
-//         addErrorBorder("email");
-//         hasErrors = true;
-//       } else if (!email.value.match(emailPattern)) {
-//         displayErrorMessage("email-error", "*Email Address is not valid.");
-//         addErrorBorder("email");
-//         hasErrors = true;
-//       }
-  
-//       if (message.value.length < 40 ) {
-//         displayErrorMessage("msg-field-error", "*Message should be at least 40 characters long.");
-//         addErrorBorder("msg-field");
-//         hasErrors = true;
-//       }
-  
-//       if (hasErrors) {
-//         return;
-//       }
-//       function displayErrorMessage(elementId, message) {
-//         const errorElement = document.getElementById(elementId + "-error");
-//         errorElement.textContent = message;
-//         errorElement.style.display = "block";
-//       }
-    
-//       function addErrorBorder(inputId) {
-//         const inputField = document.getElementById(inputId);
-//         inputField.classList.add("error-border");
-//       }
-    
-//       function clearErrorMessages() {
-//         const errorElements = document.getElementsByClassName("error-message");
-//         for (let i = 0; i < errorElements.length; i++) {
-//           errorElements[i].textContent = "";
-//           errorElements[i].style.display = "none";
-//         }
-    
-//         const inputFields = document.querySelectorAll(".input-row input");
-//         for (let i = 0; i < inputFields.length; i++) {
-//           inputFields[i].classList.remove("error-border");
-//         }
-//       }
-    
-//       function clearFormFields() {
-//         form.reset();
-//       }
-//       // If there are no errors, submit the form programmatically
-//       form.submit();
-  
-//       // Clear the form fields
-//       clearFormFields();
-//     });
-//   });
-  
   
  
   
