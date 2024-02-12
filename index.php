e4c1653a23140a3cc0fda544c1eb3f987bfad831
deanyosla <146840740+deanyosla@users.noreply.github.com>
2024-02-12 10:24:33 +0000

    initial php rebuild commit

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
             />
             
             <p>
-              Reconstructed the <strong>Netmatters Webpage</strong> from the ground up, employing a meticulous approach with a focus on responsible design principles. Utilized a technology stack comprising <strong>HTML</strong>, <strong>CSS</strong>, <strong>SASS</strong>, and <strong>JavaScript</strong> to ensure a robust and visually appealing outcome.
+              Reconstructed the <strong>Netmatters Webpage</strong> from the ground up, employing a meticulous approach with a focus on responsible design principles. Utilized a technology stack comprising <strong>HTML</strong>, <strong>CSS</strong>, <strong>SASS</strong>, <strong>JavaScript</strong>, <strong>PHP</strong> to ensure a robust and visually appealing outcome.
             </p>
             <p>
               Ensured faithful adherence to the original aesthetics, emphasizing a responsible design framework for cohesiveness with the brand identity and optimal user experience.
@@ -148,8 +148,9 @@
             <span class="code-icons">
               <i class="fa-brands fa-html5"></i>
               <i class="fa-brands fa-css3-alt"></i>
-              <i class="fa-brands fa-sass"></i
-            ><i class="fa-brands fa-js"></i>
+              <i class="fa-brands fa-sass"></i>
+              <i class="fa-brands fa-js"></i>
+              <i class="fa-brands fa-php"></i>
           </span>
 
             <a
@@ -183,14 +184,31 @@
           </div>
           <div class="tab-3">
             <img
-              class="project-img"
-              src="images/img-4.jpg"
-              alt="Placeholder image for a project"
-            />
-            <p>Project coming soon...</p>
-            <a href="#" class="project-btn"
-              >View Project <i class="fa-solid fa-arrow-right"></i
-            ></a>
+            class="project-img"
+            src="images/contact.png"
+            alt="Netmatters PHP Assessment"
+          />
+          <p>
+            Reconstructed the <strong>Netmatters</strong> Contact page from the ground up, implementing PHP and JavaScript form validation. Utilized a technology stack comprising <strong>HTML</strong>, <strong>CSS</strong>, <strong>SASS</strong>, <strong>JavaScript</strong>, <strong>PHP</strong> to ensure a robust and visually appealing outcome.
+          </p>
+          <p>
+            Ensured faithful adherence to the original aesthetics, emphasizing a responsible design framework for cohesiveness with the brand identity and optimal user experience.
+          </p>
+          
+          <span class="code-icons">
+            <i class="fa-brands fa-html5"></i>
+            <i class="fa-brands fa-css3-alt"></i>
+            <i class="fa-brands fa-sass"></i>
+            <i class="fa-brands fa-js"></i>
+            <i class="fa-brands fa-php"></i>
+        </span>
+
+          <a
+            href="http://netmatters.andrejs-volskis.netmatters-scs.co.uk/contact-us.php"
+            class="project-btn"
+            target="_blank"
+            >View Project <i class="fa-solid fa-arrow-right"></i
+          ></a>
           </div>
           <div class="tab-4">
             <img
